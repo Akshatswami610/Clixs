d88304db8cde14461923e83834f9fd6137b3fa93
Akshat Swami <akshatswami0610@gmail.com>
2025-12-21 19:59:37 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ItemsListPage from "./pages/marketplace/ItemsListPage";
@@ -8,17 +8,13 @@ import ProfilePage from "./pages/auth/ProfilePage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/items" element={<ItemsListPage />} />
-        <Route path="/items/:id" element={<ItemDetailPage />} />
-
-        {/* Auth */}
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/items" element={<ItemsListPage />} />
+      <Route path="/items/:id" element={<ItemDetailPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
   );
 }
 
