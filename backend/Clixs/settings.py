14817099ae2cd9e9e9f59fcb4fecec1bb8b2314f
Akshat Swami <akshatswami0610@gmail.com>
2025-12-22 20:19:50 +0530

    Created frontend.

--- backend/Clixs/settings.py
+++ backend/Clixs/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "Clixs.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "frontend"],
+        "DIRS": [BASE_DIR.parent / "frontend"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
