750facbb9753a37a4e25187db1c8f46f3c5f5067
Akshat Swami <akshatswami0610@gmail.com>
2026-01-10 00:54:29 +0530

    Update asgi.py

--- backend/Clixs/asgi.py
+++ backend/Clixs/asgi.py
@@ -1,15 +1,18 @@
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
-import api.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Clixs.settings")
 
+django.setup()  # 🔥 REQUIRED
+
+import api.routing  # import AFTER django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(api.routing.websocket_urlpatterns),
+    "websocket": URLRouter(
+        api.routing.websocket_urlpatterns
+    ),
 })
-
