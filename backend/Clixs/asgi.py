be4652e91a2257a7df8689a7b0ab990697a74d6a
Akshat Swami <akshatswami0610@gmail.com>
2026-01-14 20:48:02 +0530

    Update asgi.py

--- backend/Clixs/asgi.py
+++ backend/Clixs/asgi.py
@@ -4,11 +4,10 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from api.middleware import JwtAuthMiddleware
-import api.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Clixs.settings")
 django.setup()
+from api.middleware import JwtAuthMiddleware
+import api.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
